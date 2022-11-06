d850dc39dc905fb020aba0a3292abef66313c8a8
Hugo Gabriel Vega <gabrielv98@hotmail.com>
2022-11-05 22:53:59 -0300

    tp3 solved

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,57 +1,28 @@
-import { React,useEffect } from "react";
-import {Grid,Paper,Box,Typography} from '@mui/material';
-import { Link } from "react-router-dom";
-import { Button } from '@mui/material';
-import { ListAlt, ManageSearch } from '@mui/icons-material';
-import { useSelector, useDispatch } from 'react-redux';
-import { appSelector, appActions } from '../../redux/appRedux';
+import React, { useEffect } from "react";
+import { Grid, Paper, Box, Avatar,Modal,Textfield } from "@mui/material";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
+import TodoImg from "../../assets/images/todo.jpg";
+import FetchlistImg from "../../assets/images/fetchlist.jpg";
 
 
-
-
+import Typography from "@mui/material/Typography";
+import { color } from "@mui/system";
 
 const Dashboard = () => {
-  const dispatch = useDispatch()
-  useEffect(()=>{
-    dispatch(appActions.setPageTitle('TABLERO'))
-  
-   },[])
 
-  const todo = useSelector(appSelector.todo)
+  
+  const navigate = useNavigate();
   return (
-   <Grid container spacing={3}>
-    <Grid item md={6} xs={12}>
-     <Paper sx={{p: 2}}>
-     <Box>
-        <Typography sx={{fontSize:18,fontWeight:700}}>
-            Te quedan {todo.filter((todo)=>!todo.completed).length} tareas por terminar
-        </Typography> 
-        </Box>
-       <Box>
-         <Button component={Link} to="/todo">
-         <ListAlt/>
-            Todo
-          </Button>
-        </Box>
-     </Paper>
+    <Grid container spacing={3}>
+      <Grid item md={12} xs={12}>
+        <Paper sx={{ p: 2 }}>
+          <Box>Dashboard</Box>
+        </Paper>
+      </Grid>
+     
+      
     </Grid>
-    <Grid item md={6} xs={12}>
-     <Paper sx={{p: 2}}>
-      <Box>
-       <Typography sx={{fontSize:18,fontWeight:700}}>
-            Tienes {todo.filter((todo)=>todo.completed).length} tareas completadas
-        </Typography> 
-       </Box>
-       <Box>
-        <Button component={Link} to="/fetch-list">
-          <ManageSearch/>
-          FetchList
-        </Button>
-       </Box>
-     </Paper>
-    </Grid>
-   </Grid>
   );
 };
-
 export default Dashboard;
